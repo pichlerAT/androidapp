99dd2e8673c0777e304b52f27b392dac11d4e359
Edwin Pichler <edwin.pichler@gmx.at>
2016-05-01 12:47:08 +0200

    Added listview to TaskFragment

---
+++ app/src/main/java/fry/oldschool/TaskAdapter.java
@@ -0,0 +1,73 @@
+package fry.oldschool;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Edwin Pichler on 01.05.2016.
+ */
+public class TaskAdapter extends BaseAdapter{
+
+    public ToDoList[] list;
+    Context ctx = App.getContext();
+
+    public TaskAdapter(ToDoList[] list){
+        this.list = list;
+    }
+
+    @Override
+    public ToDoList getItem(int position){
+        return list[position];
+    }
+
+    @Override
+    public int getCount() {
+        return list.length;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View res;
+        if (convertView == null){
+            res = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_task_listtemplate, parent, false);
+        }
+        else{
+            res = convertView;
+        }
+        ToDoList item = getItem(position);
+
+        TextView header = (TextView) res.findViewById(R.id.textview_listtemplate_header);
+        header.setText(item.name);
+
+        LinearLayout entries = (LinearLayout) res.findViewById(R.id.linearlayout_listtemplate_id);
+
+        for (int i=0; i<item.state.length; i++){
+            CheckBox cb = new CheckBox(ctx);
+            cb.setText(item.task[i]);
+            if (item.state[i] == 1)
+                cb.setChecked(true);
+            else
+                cb.setChecked(false);
+            entries.addView(cb);
+
+        }
+
+        return res;
+    }
+}
--- app/src/main/java/fry/oldschool/TaskFragment.java
+++ app/src/main/java/fry/oldschool/TaskFragment.java
@@ -1,22 +1,35 @@
 package fry.oldschool;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
+import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ListView;
+
 
 /**
  * Created by Edwin Pichler on 28.04.2016.
  */
-public class TaskFragment extends Fragment{
+public class TaskFragment extends ListFragment{
+    Context ctx = App.getContext();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_task, container, false);
 
+        ToDoList item1 = ToDoList.create("ToDo list #1", 1);
+        item1.setAtPosition(0, "Buy bananas from the store", (byte) 1);
 
+        ToDoList item2 = ToDoList.create("ToDo list #2", 1);
+        item2.setAtPosition(0, "Call my mum", (byte) 0);
+        ToDoList[] list = {item1, item2};
+
+        ListView lv = (ListView) rootView.findViewById(R.id.listview_task_id);
+        TaskAdapter adapter = new TaskAdapter(list);
+        lv.setAdapter(adapter);
+
+        /*
         Button button_task_new_todolist = (Button) rootView.findViewById(R.id.button_task_new_todolist);
         button_task_new_todolist.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,7 +38,7 @@ public class TaskFragment extends Fragment{
                 fm.beginTransaction().replace(R.id.frame_fragment_main, new TaskCreateFragment()).commit();
             }
         });
-
+        */
         return rootView;
     }
 }
