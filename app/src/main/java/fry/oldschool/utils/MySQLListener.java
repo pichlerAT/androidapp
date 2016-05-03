51f5e3bbe0490587188f8affb0b5abf803f2d949
Stefan <stefan.fragner95@gmail.com>
2016-05-03 20:53:05 +0200

    MySQLListener

--- app/src/main/java/fry/oldschool/utils/MySQLListener.java
+++ app/src/main/java/fry/oldschool/utils/MySQLListener.java
@@ -5,11 +5,11 @@ public interface MySQLListener {
     /**
      * @param arg
      * "tdl_create" a ToDoList has been created
-     * "tdl_update" a ToDoList has been updated
-     * "tdl_delete" a ToDoList has been deleted
-     * "tdl_entry_create" an ToDoList entry has been created
-     * "tdl_entry_update" an ToDoList entry has been updated
-     * "tdl_entry_delete" an ToDoList entry has been deleted
+     * "tdl_update" a ToDoList has been updated ( includes all entries )
+     * "tdl_delete" a ToDoList has been deleted ( includes all entries )
+     * "tdl_entry_create" a ToDoList entry has been created
+     * "tdl_entry_update" a ToDoList entry has been updated
+     * "tdl_entry_delete" a ToDoList entry has been deleted
      * "tdl_load" the ToDoLists have been loaded from the server
      * "sync_all" all Lists have been synchronized
      * "sync_tdl" the ToDoLists have been synchronized
