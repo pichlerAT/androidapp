70f4e6dba318306e2cf40ba6477e0914a0658a0f
Stefan <stefan.fragner95@gmail.com>
2016-06-09 18:25:35 +0200

    Redundant row deleted

--- app/src/main/java/fry/oldschool/utils/ConnectionManager.java
+++ app/src/main/java/fry/oldschool/utils/ConnectionManager.java
@@ -69,7 +69,6 @@ public class ConnectionManager extends MySQL {
             }
             System.out.println("------------------------------------------");
             br.close();
-            sync();
 
         }catch(IOException ex) {
             ex.printStackTrace();
